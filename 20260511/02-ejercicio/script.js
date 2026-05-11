236cfd485d83e0157acc66d86223126709c7b313
danielapaz11 <laudani346@hotmail.com>
2026-05-11 22:02:02 +0200

    Correccion Value

--- 20260511/02-ejercicio/script.js
+++ 20260511/02-ejercicio/script.js
@@ -6,7 +6,7 @@ document.getElementById('pantalla-cesta').textContent = carrito.join("-");
 //La funcion para agregar cosas nuevas a la lista
 function AñadirAlCarrito(){
     //Atrapamos lo que escribe el usuario
-    let item = document.getElementById('nuevo-item').ariaValueMax;
+    let item = document.getElementById('nuevo-item').value;
     //Magia de Array .push() mete el nuevo elemento al final de la lista
     carrito.push(item);
     //Volvemos a pintar la cesta para que se ariaValueMax
